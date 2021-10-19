087cfa9fe908c983284e6447c7c050596c75d909
udaikue <ikueikue@gmail.com>
2021-10-19 16:23:50 +0900

    Diary一覧・新規作成画面を大まかに実装

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -7,13 +7,29 @@ class DiariesController < ApplicationController
 
   def show; end
 
-  def new; end
+  def new
+    @diary = Diary.new
+  end
 
-  def create; end
+  def create
+    @diary = Diary.new(diary_params)
+
+    if @diary.save
+      redirect_to diaries_path
+    else
+      render :new
+    end
+  end
 
   def edit; end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def diary_params
+    params.require(:diary).permit(:comment, :open)
+  end
 end
