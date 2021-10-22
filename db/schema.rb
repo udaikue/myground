9b177b862deaf212c5dfaf0f74a5b7e9b366f853
udaikue <ikueikue@gmail.com>
2021-10-22 10:59:57 +0900

    Teamモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -1,10 +1,18 @@
-ActiveRecord::Schema.define(version: 2021_10_14_114631) do
+ActiveRecord::Schema.define(version: 2021_10_22_013144) do
 
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "diaries", force: :cascade do |t|
     t.boolean "open", default: false, null: false
     t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.integer "league_id", null: false
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
