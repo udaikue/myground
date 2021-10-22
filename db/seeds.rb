21702305906eb3d8d3ce725d721138d66ed8176c
udaikue <ikueikue@gmail.com>
2021-10-22 13:02:41 +0900

    seeds.rbにチームデータを登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,54 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Team.create(
+  [
+    {
+      league_id: 1,
+      name: '巨人'
+    },
+    {
+      league_id: 1,
+      name: 'ヤクルト'
+    },
+    {
+      league_id: 1,
+      name: 'DeNA'
+    },
+    {
+      league_id: 1,
+      name: '中日'
+    },
+    {
+      league_id: 1,
+      name: '阪神'
+    },
+    {
+      league_id: 1,
+      name: '広島'
+    },
+    {
+      league_id: 2,
+      name: '日本ハム'
+    },
+    {
+      league_id: 2,
+      name: '楽天'
+    },
+    {
+      league_id: 2,
+      name: '西武'
+    },
+    {
+      league_id: 2,
+      name: 'ロッテ'
+    },
+    {
+      league_id: 2,
+      name: 'オリックス'
+    },
+    {
+      league_id: 2,
+      name: 'ソフトバンク'
+    }
+  ]
+)
