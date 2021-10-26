7f7020e38e71cb5bff0396e046826c7da78f6d4f
udaikue <ikueikue@gmail.com>
2021-10-26 22:50:10 +0900

    試合結果を表示

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,19 @@
 class GamesController < ApplicationController
   def index
+    @games = Game.order(:id)
+    # @game_date = @games.select(:date).distinct
   end
 
   def show
+    @game = Game.find(params[:id])
   end
 
   def create
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:date)
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 class Game < ApplicationRecord
+  # バリデーションを記述
 end
