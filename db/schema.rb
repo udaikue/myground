436a52d8d1d6afcc45d6c3084b1e692e49688d48
udaikue <ikueikue@gmail.com>
2021-11-19 11:18:56 +0900

    Linkモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_03_121110) do
+ActiveRecord::Schema.define(version: 2021_11_19_021153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,12 @@ ActiveRecord::Schema.define(version: 2021_11_03_121110) do
     t.string "saving_pitcher"
     t.string "homerun_home"
     t.string "homerun_visitor"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "links", force: :cascade do |t|
+    t.string "url", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
