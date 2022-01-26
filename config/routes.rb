e5d889039bd9706c32ff63d91d4dd413c8c692d8
udaikue <ikueikue@gmail.com>
2022-01-26 23:37:38 +0900

    rootの設定、usernameを使うよう変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,20 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'diaries#index'
-
-    namespace :api do
-      resources :games, only: %i[index]
-      resources :diaries, only: %i[show]
+  devise_scope :user do
+    unauthenticated :user do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
     end
+  end
 
-    scope ':username' do
-      resources :diaries
-    end
+  root to: 'diaries#index', as: :authenticated_root
+
+  namespace :api do
+    resources :games, only: %i[index]
+    resources :diaries, only: %i[show]
+  end
+
+  scope ':username' do
+    resources :diaries
+  end
 end
