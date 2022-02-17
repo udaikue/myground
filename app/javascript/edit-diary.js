f287a33e2f5ec77ef71ae9360e0d2bf47fe76a92
udaikue <ikueikue@gmail.com>
2022-02-18 07:51:04 +0900

    edit画面のパスの取得方法変更

--- app/javascript/edit-diary.js
+++ app/javascript/edit-diary.js
@@ -5,8 +5,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const selector = '#js-edit-diary'
   const diaries = document.querySelector(selector)
   if (diaries) {
+    // const diariesPath = diaries.dataset.diariesPath
+    const diaryId = diaries.dataset.diaryId
+    const diariesEditPath = `${diaries.dataset.diariesPath}/${diaryId}`
     new Vue({
-      render: (h) => h(EditForm)
+      render: (h) => h(EditForm, { props: { diaryId, diariesEditPath } })
     }).$mount(selector)
   }
 })
