4dedfbbd041332d735984d4498e0aea40efce5f3
udaikue <ikueikue@gmail.com>
2022-03-17 22:20:13 +0900

    Replace factories directory

--- test/factories/diaries.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :suzuki_mc20220316, class: Diary do
-    published { 'true' }
-    comment { '坂倉と小園が好調。
-      栗林が失点したのが気になるけど、床田よかった〜！' }
-    association: user, factory: :suzuki
-    association: game, factory: :mc20220316
-  end
-end
