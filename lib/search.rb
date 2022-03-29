852b7bba4dd341ea5597ab149a5a3fa650d2a967
udaikue <ikueikue@gmail.com>
2022-03-29 15:11:25 +0900

    Fix rake task

--- lib/search.rb
+++ lib/search.rb
@@ -3,14 +3,15 @@
 require 'net/http'
 
 class Search
+  attr_reader :scrapings
+
   def initialize
-    # @scrapings = Scraping.where('game_date = ? and done = ?', DateTime.now, false)
-    @scrapings = Scraping.where('game_date = ? and done = ?', '2022-3-27', false)
+    @scrapings = Scraping.where('game_date = ? and done = ?', DateTime.now, false)
+    # @scrapings = Scraping.where('game_date = ? and done = ?', '2022-3-26', false)
   end
 
   def responsed_urls
     responsed_urls = []
-    @scrapings
     @scrapings.each do |scraping|
       response = Net::HTTP.get_response(URI.parse(scraping.url))
       responsed_urls << scraping.url if response.code == '200'
