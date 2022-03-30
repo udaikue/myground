07e94855e262227c0d80a78b8c0791815db6816c
udaikue <ikueikue@gmail.com>
2022-03-30 22:56:07 +0900

    Fix condition branch of next

--- lib/url.rb
+++ lib/url.rb
@@ -19,12 +19,12 @@ class URL
 
   def today_cards
     today_element = @schedule.grep(/#{DateTime.now.month}月#{DateTime.now.day}日*/)
-    # today_element = @schedule.grep(/3月26日*/)
+    # today_element = @schedule.grep(/3月30日*/)
 
     today_cards = []
     today_element.to_s.gsub(/\d+?月+\d+?日\(.\)|\d+?|京セラD大阪|楽天生命パーク|中止/, '').scan(/巨|ヤ|D|中|阪|広|日|楽|西|ロ|オ|ソ/).each_slice(2) do |g|
       path = DateTime.now.strftime('%y%m%d') + g.join('-')
-      # path = '220326' + g.join('-')
+      # path = '220331' + g.join('-')
       url = "https://baseball-freak.com/game/#{path}.html"
       today_cards << url.gsub(/巨|ヤ|D|中|阪|広|日|楽|西|ロ|オ|ソ/,
                               '巨' => 'g',
