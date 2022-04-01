6efb1c8a25020ff8f1b821314761d782a0ceb20e
teppei machida <machida@fjord.jp>
2022-04-01 12:10:55 +0900

    scssのファイルの配置を整理した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def has_header?
+    user_signed_in? || params[:controller] == 'diaries' || params[:controller] == 'home'
+  end
+
   def my_diaries_index?
     return current_user&.username == params[:username] || !params[:username]
   end
