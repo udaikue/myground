ea4dc132eabd7361bd3b1b9c6381e1c0243d5bf7
udaikue <ikueikue@gmail.com>
2022-04-04 00:20:20 +0900

    Remove unnecessary return

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,14 +6,14 @@ module ApplicationHelper
   end
 
   def my_diaries_index?
-    return current_user&.username == params[:username] || !params[:username]
+    current_user&.username == params[:username] || !params[:username]
   end
 
   def my_diary?
-    return current_user&.username == params[:username]
+    current_user&.username == params[:username]
   end
 
   def is_first_tab_active?
-    return request.path == '/diaries' || request.path == '/'
+    request.path == '/diaries' || request.path == '/'
   end
 end
