4d454553a1f82727f3872a654c88610f7bf2b8d6
udaikue <ikueikue@gmail.com>
2022-04-04 15:27:20 +0900

    Add ActionMailer setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "myground_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'example.com' }
 
+  config.action_mailer.raise_delivery_errors = true
+  host = 'my-ground.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => host,
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
