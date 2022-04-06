6e1fb7be9d0027dd8ee0fa3c5ff797e0d94536fa
udaikue <ikueikue@gmail.com>
2022-04-06 15:30:35 +0900

    Add fixtures data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,9 @@ ActiveRecord::FixtureSet.create_fixtures 'db/fixtures', %i[
   ballparks
   games
   scores
+  diaries
+  links
 ]
+
+User.create!(id: 1, email: 'test1@example.com', username: 'test_user1', password: 'password')
+User.create!(id: 2, email: 'test2@example.com', username: 'test_user2', password: 'password')
