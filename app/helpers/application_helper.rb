aafb709283cdb1b1d7a083796d7b7a7694b91bf5
udaikue <ikueikue@gmail.com>
2022-04-07 14:57:25 +0900

    Fix styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,20 @@ module ApplicationHelper
   def first_tab_active?
     request.path == '/diaries' || request.path == '/'
   end
+
+  def index_game_date(game)
+    "#{game.date.year}/#{game.date.month}/#{game.date.day}"
+  end
+
+  def index_game_card(game)
+    "#{Team.find(game.team_home_id).name}VS#{Team.find(game.team_visitor_id).name}"
+  end
+
+  def index_game_ballpark(game)
+    "@#{Ballpark.find(game.ballpark_id).name}"
+  end
+
+  def index_diary_username(diary)
+    User.find(diary.user_id).username
+  end
 end
