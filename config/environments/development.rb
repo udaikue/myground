d740a36598c413e08d4028203678745972b59ec0
udaikue <ikueikue@gmail.com>
2022-04-11 12:56:30 +0900

    Change mailer setting of development environment to Gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  
+
   config.action_mailer.default_url_options = { host: 'example.com' }
 
   # Print deprecation notices to the Rails logger.
@@ -77,4 +77,13 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:"smtp.gmail.com",
+    domain: 'gmail.com',
+    port:587,
+    user_name: Rails.application.credentials.gmail[:user_name],
+    password: Rails.application.credentials.gmail[:password],
+    authentication: :login
+  }
 end
