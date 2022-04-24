cead02fdc865a9656dd67d5ea16006b00f8d63d2
udaikue <ikueikue@gmail.com>
2022-04-24 11:26:35 +0900

    Change order of ActionMailer setting description

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   host = 'my-ground.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
@@ -76,7 +77,6 @@ Rails.application.configure do
     authentication: :plain,
     enable_starttle_auto: true
   }
-  config.action_mailer.delivery_method = :smtp
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
